d90644c978e234a736bd91b2536cd3d691a3d927
Aastharani0720 <aastha.rani.cse.2025@miet.ac.in>
2026-08-26 10:40:07 +0530

    Add files via upload

--- insertion.c
+++ insertion.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    int arr[100] = {10, 20, 30, 40, 50}; 
-    int size = 5;                        
-    int num = 25;                        
-    int pos = 3;                         
-    
-    // Shift elements to the right to make space
-    for (int i = size; i >= pos; i--) {
-        arr[i] = arr[i - 1];
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
     }
-    
-    arr[pos - 1] = num;  
-    size++;             
-    
-    printf("Array after insertion: ");
-    for (int i = 0; i < size; i++) {
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    insertionSort(arr, n);
+    printArray(arr, n);
     
     return 0;
 }
