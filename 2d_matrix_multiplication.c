ba9258a11271597c83c784931c9b55becca94e69
Aastharani0720 <aastha.rani.cse.2025@miet.ac.in>
2026-08-26 10:15:44 +0530

    Add files via upload

---
+++ 2d_matrix_multiplication.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int r1, c1, r2, c2;
+
+    printf("Enter rows and columns for first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns for second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if (c1 != r2) {
+        printf("Error! Column of first matrix not equal to row of second.\n");
+        return 1;
+    }
+
+    int a[r1][c1], b[r2][c2], result[r1][c2];
+
+    printf("\nEnter elements of matrix 1:\n");
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            printf("Enter element a%d%d: ", i + 1, j + 1);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of matrix 2:\n");
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            printf("Enter element b%d%d: ", i + 1, j + 1);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            result[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            for (int k = 0; k < c1; ++k) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("\nOutput Matrix:\n");
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            printf("%d\t", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
